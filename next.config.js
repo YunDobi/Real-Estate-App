7be45aac754293cd74162a1800902ec4b13122d3
Yunchoel Lee <yun@Yunchoels-MacBook-Air.local>
2022-06-25 19:49:50 -0700

    change the next.config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['bayut-production.s3.eu-central-1.amazonaws.com']
+  },
+  experimental: {
+    forceSwcTransforms: true,
   }
 }
 
